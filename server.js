8d7f2c106dadca2ad5ff3932f497cf0b7bce377e
jsavidan <j.savidan@gmail.com>
2020-05-04 13:54:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('No Music Available Yet');
+  res.json({"status":"KO"});
 });
 
 app.listen(3000, function () {
